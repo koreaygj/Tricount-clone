7f66157d8d4510c6c1835dd602e46287b4d7b855
koreaygj <ygj1828@gmail.com>
2024-01-08 16:01:41 +0900

    init:spring web project

---
+++ src/main/java/groom/tricountClone/TricountCloneApplication.java
@@ -0,0 +1,13 @@
+package groom.tricountClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricountCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TricountCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/groom/tricountClone/TricountCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package groom.tricountClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TricountCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
